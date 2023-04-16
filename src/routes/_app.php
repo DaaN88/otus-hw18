99f7273398e635e82936681a6e7eb95d5ff51f7c
DaaN88 <56934683+DaaN88@users.noreply.github.com>
2023-04-16 21:12:56 +0400

    Update _app.php

--- src/routes/_app.php
+++ src/routes/_app.php
@@ -13,9 +13,11 @@ app()->get('/test', 'TestsController@test');
 app()->get(pattern: '/api-doc', handler: function () {
     try {
         $openapi = \OpenApi\Generator::scan([__DIR__ . '/../controllers']);
-        header('Content-Type: application/json');
-        header('Access-Control-Allow-Headers: *');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Content-Type, api_key, Authorization');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Content-Type: application/json');
+        
         file_put_contents(filename: __DIR__ . '/../pages/openapi.json', data: $openapi->toJson());
 
         echo "Success";
